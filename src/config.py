acf921c6b9827fcf414bcc20e14eb36f0fb93875
shreyassks <skshreyas714@gmail.com>
2022-01-09 18:12:53 +0530

    augmented data with paraphrase generation

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 EMBEDDING_FILE_LOCATION = 'models/wiki-news-300d-1M.vec'
-TRAINING_DATA_LOCATION = 'data/train.csv'
+TRAINING_DATA_LOCATION = 'data/updated_train.csv'
 TEST_DATA_COMMENTS = 'data/test_set.csv'
 MODEL_LOCATION = 'models/toxicity_classifier.h5'
 TOKENIZER_LOCATION = 'models/tokenizer.pkl'
